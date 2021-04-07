792c3d74897084fb9800d9d1b61ee5730b58fa37
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-04-07 15:04:57 -0700

    Update ex03.py

--- Day00/ex03.py
+++ Day00/ex03.py
@@ -1,19 +1,22 @@
 def text_analyzer(text_to_analyze):
     c = 0, k = 0, l = 0, i = 0, j = 0
-    for char in text_to_analyze:
-        c++
-        if char.isupper():
-            k++
-        if char.islower():
-            l++
-        if char == ' ':
-            i++
-        else:
-            j++
-   print("===========The text contains", c ,"characters==========\n\n")
-   print(k ,"upper letters\n")
-   print(l ,"lower letters\n")
-   print(i ,"punctuation marks\n")
-   print(j ,"spaces")
+    try:
+        for char in text_to_analyze:
+            c++
+            if char.isupper():
+                 k++
+            if char.islower():
+                l++
+            if char == ' ':
+                i++
+            else:
+                j++
+        print("===========The text contains", c ,"characters==========\n\n")
+        print(k ,"upper letters\n")
+        print(l ,"lower letters\n")
+        print(i ,"punctuation marks\n")
+        print(j ,"spaces")
+     except:
+        printf("Please type a valid sentence to the given function!")
 
 
