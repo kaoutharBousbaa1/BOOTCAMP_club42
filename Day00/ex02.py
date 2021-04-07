92d267fd7db1e3c55e64bda64634793c2645d8f7
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-04-07 14:54:00 -0700

    Update ex02.py

--- Day00/ex02.py
+++ Day00/ex02.py
@@ -1,10 +1,11 @@
 def function(number):
-    if number == 0:
-        print("The given number is equal to zero")
-    if number % 2 == 0:
-        print("It's an even number")
-    if number % 2 != 0
-        print("It's an odd number")
-    else:
-        print("ERROR! Please enter an integer number")
+    try:
+        if number == 0:
+            print("The given number is equal to zero")
+        if number % 2 == 0:
+            print("It's an even number")
+        if number % 2 != 0
+            print("It's an odd number")
+    except ValueError:
+        print("ValueError! Please enter an integrer number")
         
