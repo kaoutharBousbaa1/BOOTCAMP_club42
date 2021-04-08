0341cc9c4ee8a1ee2bacce8156bb4fd553813c31
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-04-08 07:29:47 -0700

    Update ex09.py

--- Day00/ex09.py
+++ Day00/ex09.py
@@ -1,19 +1,18 @@
-NombreMystere = 0, NombreChoisi = 0, ContinuerPartie = 0, MAX = 0, coups = 0, MIN = 1
-while (ContinuerPartie != 0)
-  while(NombreChoisi != NombreMystere)
-
-      srand(time(NULL))
-      NombreMystere = (rand() % (MAX - MIN +1)) + MIN
-      NombreChoisi = input("Quel est le nombre? ")
+printf("This is an interactive guessing game! \n You have to enter a number between 1 and 99 to find out the secret number.\n Type '0' to end the game.\n Good luck!\n")
+NombreMystere = 0, NombreChoisi = 0, ContinuerPartie = 0, coups = 0
+while ContinuerPartie != 0:
+  while NombreChoisi != NombreMystere:
+      NombreMystere = randint(1,99)
+      NombreChoisi = int(input("Quel est le nombre? "))
       
-      if (NombreMystere > NombreChoisi) 
+      if NombreMystere > NombreChoisi:
           printf("C'est plus!\n")
           coups++
-      else if(NombreMystere < NombreChoisi) 
+      elif NombreMystere < NombreChoisi:
           printf("C'est moins!\n")
           coups++
-      else
-          printf("Bravo!! Vous avez trouvez le nombre mystere! avec un nombre des coups %d\n",&coups)
-          ContinuerPartie = input("Voulez-vous continuer?(0/1)")
+      else:
+          printf("Bravo!! Vous avez trouvez le nombre mystere! avec un nombre des coups %d\n", %coups)
+          ContinuerPartie = int(input("Voulez-vous continuer?(0/1)"))
        
 
