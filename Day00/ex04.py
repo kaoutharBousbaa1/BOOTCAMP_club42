b3101e2c46fc5cb762be23afae7b38ba49e525bd
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-04-08 04:44:05 -0700

    Update ex04.py

--- Day00/ex04.py
+++ Day00/ex04.py
@@ -1,12 +1,16 @@
-num1 = int(input('Enter First number: '))
-num2 = int(input('Enter Second number '))
-addition = num1 + num2
-difference = num1 - num2
-product = num1 * num2
-division = num1 / num2
-modulus = num1 % num2
-print('Sum of ',num1 ,'and' ,num2 ,'is :',addition)
-print('Difference of ',num1 ,'and' ,num2 ,'is :',difference)
-print('Product of' ,num1 ,'and' ,num2 ,'is :',product)
-print('Division of ',num1 ,'and' ,num2 ,'is :',division)
-print('Modulus of ',num1 ,'and' ,num2 ,'is :',modulus)
+try:
+  num1 = int(input('Type a integer number please: '))
+  num2 = int(input('Type another one: (must be an integer)'))
+  addition = num1 + num2
+  difference = num1 - num2
+  product = num1 * num2
+  division = num1 / num2
+  modulus = num1 % num2
+  print('Sum of ',num1 ,'and' ,num2 ,'is :',addition, end = '\n')
+  print('Difference of ',num1 ,'and' ,num2 ,'is :',difference, end = '\n')
+  print('Product of' ,num1 ,'and' ,num2 ,'is :',product, end = '\n')
+  print('Division of ',num1 ,'and' ,num2 ,'is :',division, end = '\n')
+  print('Modulus of ',num1 ,'and' ,num2 ,'is :',modulus)
+  break
+  except ValueError:
+    print('You have to type an integer number! ')
