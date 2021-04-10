697bc6c3a5d401f9595f9ddbb53e4c6e96190282
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-04-09 17:53:01 -0700

    Update ex10.py

--- Day00/ex10.py
+++ Day00/ex10.py
@@ -1,15 +1,11 @@
+def ft_progress(lst):
+    
+
+
 listy = range(1000)
 ret = 0
 for elem in ft_progress(listy):
     ret += (elem + 3) % 5
     sleep(0.01)
-print()
-print(ret)
-
-listy = range(3333)
-ret = 0
-for elem in ft_progress(listy):
-    ret += elem
-    sleep(0.005)
 print()
 print(ret)
