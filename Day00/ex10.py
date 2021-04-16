dee870267a87e5e84d6ff294d29004f7888f0a44
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-04-16 06:43:10 -0700

    Update ex10.py

--- Day00/ex10.py
+++ Day00/ex10.py
@@ -1,5 +1,20 @@
+from time import sleep, time
 def ft_progress(lst):
-    
+    lst = list(lst)[1:]
+    taille = len(lst)
+    start = time()
+    for i in lst:
+        now = time()
+        time_elapsed = now - start
+        time_estimated = 0
+        time_estimated = time_elapsed * taille / i
+        print("ETA : ", end="")
+        print("{:.2f}".format(time_estimated) + "s", end="")
+        print("[{:>3.0f}%]".format(i * 100 / taille), end="")
+        print("[{:=>{}}>{:<{}}]".format("", int((i/taille)*24), "", 24 - int((i/taille)*24)), end="")
+        print("{:{}}/{}".format(i, len(str(taille)), taille ) end="")
+        print("| elapsed time {:.2f}".format(time_elapsed) + "s", end="")  
+        yield i
 
 
 listy = range(1000)
